2881b5d2ea6c3b1764db76032da5e32e119b981a
JiaWei Lee <gsdukbh@gmail.com>
2021-08-05 20:17:28 +0800

    add

--- script/bash.sh
+++ script/bash.sh
@@ -8,21 +8,22 @@ else
   nl=$(echo "${_date}-${m}")
 fi
 images="images/4k_$nl"
+mkdir "up"
 echo "压缩所有的4k图片"
-tar -czf 4k_wallpaper_all.tar.gz images/4k_*
+tar -czf up/4k_wallpaper_all.tar.gz images/4k_*
 echo "压缩完成--4k_wallpaper_all.tar.gz"
 echo "压缩所有的原图图片"
-tar -czf wallpaper_all.tar.gz images/bing_*
+tar -czf up/wallpaper_all.tar.gz images/bing_*
 echo "压缩完成--wallpaper_all.tar.gz"
 
 if [ $(echo $(date +"%m-%d")) == "01-01" ]; then
   echo "打包上一年图片"
-  tar -czf 4k_wallpaper_ $(echo $(_date)).tar.gz images/4k_$(echo $(_date))*
-  tar -czf wallpaper_ $(echo $(_date)).tar.gz images/bing_$(echo $(_date))*
+  tar -czf up/4k_wallpaper_ $(echo $(_date)).tar.gz images/4k_$(echo $(_date))*
+  tar -czf up/wallpaper_ $(echo $(_date)).tar.gz images/bing_$(echo $(_date))*
   elif [ ! -f $($images)  ]; then
     echo "开始当前压缩上个月份的图片"
-    tar -czf 4k_wallpaper_$(echo $(nl))_.tar.gz images/4k_$(echo $(nl))*
-    tar -czf wallpaper_$(echo $(nl))_.tar.gz images/bing_$(echo $(nl))*
+    tar -czf up/4k_wallpaper_$(echo $(nl))_.tar.gz images/4k_$(echo $(nl))*
+    tar -czf up/wallpaper_$(echo $(nl))_.tar.gz images/bing_$(echo $(nl))*
     else
       echo "什么都不做"
 fi
