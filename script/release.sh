e3872e30552b8cdbdf33c2738d2ecd7e0de66cd7
JiaWei Lee <gsdukbh@gmail.com>
2021-08-05 21:30:46 +0800

    add

--- script/release.sh
+++ script/release.sh
@@ -2,10 +2,9 @@ _date=$(echo $(date +"%Y"))
 day=$(echo $(date +"%m"))
 m=$(($day - 1))
 if [ -$m -lt 10 ]; then
-#  nl="${_date}0${m}"
-  nl="${m}-213"
+  tagName="${_date}0${m}"
 else
-  nl="${_date}${m}"
+  tagName="${_date}${m}"
 fi
 
 if [ $(echo $(date +"%m-%d")) = "01-01" ]; then
@@ -13,7 +12,7 @@ if [ $(echo $(date +"%m-%d")) = "01-01" ]; then
    touch release.txt
    echo "打包上一年图片" >> release.txt
    else
-     echo "::set-output name=release_tag::${nl}"
+     echo "::set-output name=release_tag::${tagName}"
      touch release.txt
      echo "每月打包图片" >> release.txt
 fi
