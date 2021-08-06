034e5dda8aff92538a0f66682d2b71224fc1abe7
JiaWei Lee <gsdukbh@gmail.com>
2021-08-06 11:57:31 +0800

    add

--- script/release.sh
+++ script/release.sh
@@ -7,7 +7,7 @@ if [ $(echo $(date +"%m-%d")) = "01-01" ]; then
    else
      {
      touch release.txt
-     echo "打包$(echo $(date +"%Y"))-$(($(echo $(date +"%m")) - 1))月份的图片" >> release.txt
+     echo "打包$(echo $(date +"%Y"))-$((10#$(echo $(date +"%m")) - 1))月份的图片" >> release.txt
      }
 fi
-echo "::set-output name=release_tag::v$(echo $(date +"%Y")).$(($(echo $(date +"%m")) - 1))"
+echo "::set-output name=release_tag::v$(echo $(date +"%Y")).$((10#$(echo $(date +"%m")) - 1))"
