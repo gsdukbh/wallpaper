f9083de2ac1950d1170780665a308fc3a9224797
JiaWei <gsdukbh@gmail.com>
2023-01-03 10:55:42 +0800

    fix

--- script/bash.sh
+++ script/bash.sh
@@ -13,12 +13,12 @@ echo $nl
 images=$(ls | grep images/4k_$nl)
 
 mkdir "up"
-echo "压缩所有的4k图片"
-tar -czf up/4k_wallpaper_all.tar.gz images/4k_*
-echo "压缩完成--4k_wallpaper_all.tar.gz"
-echo "压缩所有的原图图片"
-tar -czf up/wallpaper_all.tar.gz images/bing_*
-echo "压缩完成--wallpaper_all.tar.gz"
+# echo "压缩所有的4k图片"
+# tar -czf up/4k_wallpaper_all.tar.gz images/4k_*
+# echo "压缩完成--4k_wallpaper_all.tar.gz"
+# echo "压缩所有的原图图片"
+# tar -czf up/wallpaper_all.tar.gz images/bing_*
+# echo "压缩完成--wallpaper_all.tar.gz"
 
 date_temp=$(date +"%m-%d")
 
